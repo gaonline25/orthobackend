a141bc2abfc38996eb97d04cfbe8812110f714fc
Sourav Mehta <kazuokaze00@gmail.com>
2026-09-03 21:20:58 +0530

    More cors

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,6 +37,7 @@ export default buildConfig({
     'https://frontend-self-gamma-99.vercel.app',
     'http://127.0.0.1:5500',
     'https://orthobackend-nu.vercel.app',
+    'https://ortho-fl.web.app',
   ],
   sharp,
   plugins: [],
